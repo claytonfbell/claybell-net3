ceb796b719c4a3b84517960f489df82a80a5509d
Clay Bell <clay.bell@etix.com>
2022-03-16 15:00:18 -0700

    fix: Auto restart the docker if server reboots

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ docker container stop claybellnet
 docker container rm claybellnet
 git pull
 docker build ./ -t claybellnet
-docker run -d -p 127.0.0.1:3001:3000/tcp --name=claybellnet claybellnet 
+docker run -d -p 127.0.0.1:3001:3000/tcp --name=claybellnet --restart=unless-stopped claybellnet 
