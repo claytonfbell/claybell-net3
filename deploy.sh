0e8483adc2c395dbf6e78fffa26c221f0c423cc4
Clay Bell <clay.bell@etix.com>
2024-11-21 16:52:49 -0800

    new deployment

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ docker container stop claybellnet
 docker container rm claybellnet
 git pull
 docker build ./ -t claybellnet
-docker run -d -p 10.7.7.62:3000:3000/tcp --name=claybellnet --restart=unless-stopped claybellnet 
+docker run -d -p 10.7.7.11:3000:3000/tcp --name=claybellnet --restart=unless-stopped claybellnet 
